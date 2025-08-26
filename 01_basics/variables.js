cf859c6b37a4f81d97f8b2231e7659db81504236
Vivek kumar <vivekkumar143520@gmail.com>
2025-08-26 08:15:10 +0000

    episode - 3 completed

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,10 +1,20 @@
 
 const accountId = 12345
-let accountEmail = "vivek@gamil.com"
+let accountEmail = "vivek@g.com"
 var accountPassword = ("vivekkumar")
 accountCity = ("sonbhadra")
+var accountState;
 
-accountId = 9090909
+// accountId = 9090909
 console.log(accountId);
 
+/*
+Prefer not to use "var"
+because of issue in block scope and functional scope 
+*/
 
+accountEmail = "rani@g.com"
+accountPassword = "hshssh"
+accountCity = "chopan"
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
