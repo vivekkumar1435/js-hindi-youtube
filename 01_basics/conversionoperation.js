073ed307103b1a3fcf911fbac6dfa623cdf938a2
Vivek kumar <vivekkumar143520@gmail.com>
2025-08-28 14:53:40 +0000

    conversion operations

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,28 @@
+
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// True => 1 ; False => 0
+
+let isLoggedIn =  1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => True ; 0 => False;
+// "vivek" => True
+// "" => False
+
+let someNumber = 44
+let stringNumber = Number (someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
+
+
