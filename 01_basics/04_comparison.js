da23bc52536849078b7ea6828a7f17a1bc772b00
Vivek kumar <vivekkumar143520@gmail.com>
2025-09-02 06:44:12 +0000

    comparison and strictcheck

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 < 0);
+// console.log( 2 >= 1 );
+// console.log( 2 == 3);
+// console.log( 2 != 3);
+
+console.log("2" > 1);      // avoide this type of comparison and write clean code 
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0 );
+
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined == 0);
+
+// === strictcheck
+
+console.log("2" == 2);  // true 
+console.log("2"=== 2);  // false    string is not compariable to number thats why strictcheck make them false
+
+
+
+
+
+
