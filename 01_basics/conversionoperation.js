c5f0accaa31474d47f343e3f3dc9c779508abd59
Vivek kumar <vivekkumar143520@gmail.com>
2025-09-02 05:56:59 +0000

    conversion and operation and prefix /postfix

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -25,4 +25,38 @@ let stringNumber = Number (someNumber)
 console.log(someNumber);
 console.log(typeof stringNumber);
 
+/*...................................Operations...................................*/
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// basics operations 
+
+console.log(2+2);  // addition 
+console.log(2-2);  // subtraction
+console.log(2*2);  // multiplication
+console.log(2**2); // power
+console.log(2/2);  // division
+console.log(2%2);  // remender
+ 
+// example 
+
+/* let str1 = "hello" 
+   let str2 = "vivek"
+   let str3 = str1 + str2
+   console.log(str3);
+*/
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1+2+"2");
+
+
+
+
+
+
 
