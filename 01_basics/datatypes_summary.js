52917fb8e0eec2ccbb18417595b3874834d68d67
Vivek kumar <vivekkumar143520@gmail.com>
2025-09-07 08:05:35 -0700

    Create datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,46 @@
+//   Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefine, Symbol, BigInt
+
+let score = 100
+let ScoreValue = 100.8
+
+let isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const Id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(Id === anotherId);
+
+const bigNumber = 1234567545646464146454654635456n   // use  n after number to represent as bigint
+console.log(typeof bigNumber);
+
+
+
+// Refrance (Non Primitive)
+
+// Array, Objects, Functions
+
+const Heros = ["saktiman", "naagraj", "doga"];   // use [] to represent Arrays
+
+ let myInfo = {
+    name: "vivek",    
+    age :  19,
+};                           //  use {} to represent Objects 
+
+const myFunction = function (){console.log("helo world");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
