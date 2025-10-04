4a34c643956daf3ce67d9ec1a6b2f695a3580c33
Vivek kumar <vivekkumar143520@gmail.com>
2025-10-04 08:38:25 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,4 +7,26 @@ console.log("e");
 console.log("k");
 }
 
-sayMyName()
+// sayMyName()
+
+function addTwoNumbers(number1, number2){       //(number1, number2) parameter
+         let result = (number1 + number2)
+        //   console.log("vivek")
+         return result
+         
+}
+
+ const result = addTwoNumbers(3,4)                              // (3,4) arguments
+//  console.log("return:", result);
+ 
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("please enter username");
+        return                                     // no code exicute in function after `return`
+        
+    }
+    return `${username} just Logged In`
+}
+console.log(loginUserMessage("vivek"));
