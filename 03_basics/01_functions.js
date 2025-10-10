3c24fd4eb844a646b0b6285ba2c181f3ac6599bd
Vivek kumar <vivekkumar143520@gmail.com>
2025-10-10 16:00:31 +0000

    arrow function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,3 +30,28 @@ function loginUserMessage(username = "sam"){
     return `${username} just Logged In`
 }
 console.log(loginUserMessage("vivek"));
+
+function calculateCartPrice (val1,val2,...num1){           // use `...` rest operator for packing elements .
+   return [val1,val2,num1]
+};
+console.log(calculateCartPrice(200,300,400,30,2000,199)); 
+
+let user = {
+    username:"vivek",
+    price : 199
+};
+
+function handleObject(anyObject){
+     return `username is ${anyObject.username} and the price is ${anyObject.price}`
+}
+console.log(handleObject(user));
+
+const myNewArray = [100,200,300];
+
+function arrayreturn (anyarray){
+    return anyarray[1]
+}
+
+// console.log(arrayreturn(myNewArray));
+console.log(arrayreturn([1,3,5,7]));
+
