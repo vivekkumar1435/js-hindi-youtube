78345b21d42799a17d57c9d1af8e00d6f3f15cd4
Vivek kumar <vivekkumar143520@gmail.com>
2025-10-12 14:51:51 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+    
+// }
+
+ const month = 1
+ switch (month){
+    case 1:
+        console.log("janaury")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 2:
+        console.log("feburary")
+        break;
+    case 4:
+        console.log("april")
+        break;
+        default:
+            console.log("not identified")
+            break;
+ }
