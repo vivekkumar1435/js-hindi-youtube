fc42d1e1a7cc0d80534c13669858e60307fb94aa
Vivek kumar <vivekkumar143520@gmail.com>
2025-10-21 07:49:48 -0700

    Add files via upload

---
+++ 05_itrations/Three.js
@@ -0,0 +1,24 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+const students = ["vivek", "harsh", "ratnesh","kushal", "arman", "priyanshu"];
+
+for (const students_list of students){
+     console.log(students_list);
+     
+}
+
+const greetings = "hello world!";
+
+for (const greet of greetings) {
+     console.log(greet);
+     
+}
+
+// Maps
+
+const map = new Map()
---
+++ 05_itrations/Two.js
@@ -0,0 +1,28 @@
+// while loop
+
+let index = 0
+while ( index <= 10 ) {
+    console.log(`value of index is ${index}`);
+    index = index +2
+
+}  
+
+let myArray =['flash', "batman", "superman"]
+let arr = 0
+while (arr < myArray.length)
+{
+    console.log(`value of array is ${myArray[arr]}`);
+    arr = arr+1
+    
+}
+
+
+// Do while loop
+
+let score = 0
+do{ 
+    console.log(`score is ${score}`);
+    score = score +1
+    
+} while(score <= 10)
+index = index +2
