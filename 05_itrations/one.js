8ff1d2f38b64e7c8221b3f7fcae2f969bbc1cacf
Vivek kumar <vivekkumar143520@gmail.com>
2025-10-21 14:48:15 +0000

    loop

---
+++ 05_itrations/one.js
@@ -0,0 +1,56 @@
+// for 
+
+for (let i = 0; i <= 10; i++) {
+    const element = i ;
+    if (element == 5){
+        console.log("5 is the best number")
+    }else {console.log("bad number")}
+    console.log(element);
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop value: ${i}`);
+    
+    for (let j = 0; j <=10;j++) {
+        console.log(`inner loop value ${j} and inner loop${i}`);
+        console.log(i+ '*' +j + '=' + i*j);
+        
+    }
+}
+
+
+
+let myarray = ["flash" , "batman", "superman"]
+
+for (index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+    
+}
+
+//  break and continue
+
+for (let index = 1; index <= 20; index++)
+{
+    if (index == 5){
+        console.log(`Detected 5`);
+        break;
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+
+
+
+for (let index = 1; index <= 20; index++)
+{
+    if (index == 5){
+        console.log(`Detected 5`);
+        continue;
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+}
