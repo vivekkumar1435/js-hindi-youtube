a6e5afc8237ec2656b0822ece400ff72ca8f8c06
Vivek kumar <vivekkumar143520@gmail.com>
2025-10-26 04:51:38 +0000

    four

---
+++ 05_itrations/Four.js
@@ -0,0 +1,25 @@
+/*
+const myObject = {
+    Js : "javascript",
+    CPP:"C++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+
+// For in loop
+
+for (const key  in myObject)
+{
+    console.log(`${key} is for ${myObject[key]}` );
+    
+}
+*/
+
+// loop in array 
+
+const programming = ["js", "rb", "py"]
+for (const arrloop in programming)
+{
+    console.log(programming[arrloop]);
+    
+}
